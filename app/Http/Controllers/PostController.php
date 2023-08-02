41fb2559d7af0a27b1a9c23cbf032640c8f11020
mutsuki1729 <muratsuji.kai@gmail.com>
2023-08-03 06:33:29 +0900

    add 記事投稿作成画面

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,9 +11,13 @@ class PostController extends Controller
     {
         return view('posts.index')->with(['posts' => $post->getPaginateBylimit(1)]); 
     }
-      public function show(Post $post)
+    public function show(Post $post)
     {
         return view('posts.show')->with(['post' => $post]);
+    }
+    public function create()
+    {
+        return view('posts.create');
     }
 }
   
